33f43ab6fd3088d0670f7999bf34abbd7dd1f409
Adam Williams <67129355+Adams-Galaxy@users.noreply.github.com>
2026-06-02 16:26:54 +1200

    Added untracked and .gitignore.

---
+++ scripts/packages.sh
@@ -0,0 +1,15 @@
+# Package manifest for shell bootstrap.
+# Add or remove tools here to extend the default install.
+
+MACOS_PACKAGES="git curl zsh starship zoxide atuin direnv eza neovim yazi fastfetch fzf fd ripgrep bat gh git-delta lazygit lazydocker btop"
+
+LINUX_BASE_PACKAGES="git zsh curl ca-certificates"
+LINUX_TOOL_PACKAGES="starship zoxide atuin direnv eza neovim yazi fastfetch fzf fd ripgrep bat gh lazygit lazydocker btop"
+
+APT_EXTRA_PACKAGES="delta"
+DNF_EXTRA_PACKAGES="delta"
+PACMAN_EXTRA_PACKAGES="git-delta"
+ZYPPER_EXTRA_PACKAGES="git-delta"
+APK_EXTRA_PACKAGES="git-delta"
+
+OMZ_PLUGINS="zsh-autosuggestions zsh-syntax-highlighting zsh-completions"
