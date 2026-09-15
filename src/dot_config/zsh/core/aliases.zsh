927c285d50bf3884a70af9d8e75c81affafe231d
Adam Williams <adam.g.williams.nz@gmail.com>
2026-09-15 14:56:23 +1200

    Mac additions.

--- src/dot_config/zsh/core/aliases.zsh
+++ src/dot_config/zsh/core/aliases.zsh
@@ -36,6 +36,10 @@ if ! command -v bat >/dev/null 2>&1 && command -v batcat >/dev/null 2>&1; then
   alias bat="batcat"
 fi
 
+if command -v moonlight >/dev/null 2>&1; then
+  alias ml="moonlight"
+fi
+
 # Git aliases
 alias gs="git status"
 alias ga="git add"
